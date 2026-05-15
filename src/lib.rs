cd5370e9beb82cf750f8ea4cef46a247b6f6c8de
aatran14 <tranandy012@gmail.com>
2026-05-14 20:59:20 -0700

    refactor lib.rs and main.rs for creusot

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+
+// need a way to compute how far two vectors are. in a vector db, every search compares a query vector stored against the stored vectors to find knn or some subset of that.
+
+// we are going to also use creusot to practice programming that's correect by construction. treat this as a mathematics exercise. it's daunting, but it's the right way to move forward in databases.
+
+extern crate creusot_std;
+use creusot_std::prelude::*;
+
+
+// method1: ecludian_distance_squared
+// with creusot annotations
+#[requires( // we need to promise that slides of a and b are the same length. it is too much headache to allow mismatched slices. 
+    a@.len() == b@.len()
+)] 
+
+// #[ensures(result >= 0.0)] // verify that the result will never be negative
+//  #774 on Creusot PR says Parital Ord does not have support. Come back to this later. perhaps i should write the proof for partial ord f32 floats for creusot.
+pub fn euclidian_distance_squared(a: &[f32], b: &[f32]) -> f32 {
+    let mut sum: f32 = 0.0;
+    let mut i: usize = 0;
+
+    // #[invariant(sum >= 0.0)] // has no float comparison
+    while i < a.len() {
+        let d = a[i] - b[i];
+        sum += d * d; // distance squared
+        i += 1;
+    }
+
+    sum
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
+ use tpuf_v1::euclidian_distance_squared;
 
-// need a way to compute how far two vectors are. in a vector db, every search compares a query vector stored against the stored vectors to find knn or some subset of that.
-
-// we are going to also use creusot to practice programming that's correect by construction. treat this as a mathematics exercise. it's daunting, but it's the right way to move forward in databases.
-
-extern crate creusot_std;
-use creusot_std::prelude::*;
-
-
-// method1: ecludian_distance_squared
-// with creusot annotations
-#[requires( // we need to promise that slides of a and b are the same length. it is too much headache to allow mismatched slices. 
-    a@.len() == b@.len()
-)] 
-
-#[ensures(result >= 0.0)] // verify that the result will never be negative
-pub fn euclidian_distance_squared(a: &[f32], b: &[f32]) -> f32 {
-    let mut sum: f32 = 0.0;
-    let mut i: usize = 0;
-
-    #[invariant(sum >= 0.0)]
-    while i < a.len() {
-        let d = a[i] - b[i];
-        sum += d * d; // distance squared
-        i += 1;
-    }
-
-    sum
-
-}
-
-fn main() {
-    let a = creusot_std::vec![1.0, 2.0, 3.0];
-    let b = creusot_std::vec![4.0, 5.0, 6.0];
-    let d = euclidian_distance_squared(&a, &b);
-    println!("huzzah: {}", d);
-}
+  fn main() {
+      let a = vec![1.0, 2.0, 3.0];
+      let b = vec![4.0, 5.0, 6.0];
+      let d = euclidian_distance_squared(&a, &b);
+      println!("huzzah: {}", d);
+  }
