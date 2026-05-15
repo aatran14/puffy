b5ad6aec670425011d80b67b214cda51514d6154
aatran14 <tranandy012@gmail.com>
2026-05-15 12:19:11 -0700

    add the querry function. update the main.rs to demo. Pending LSM.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
- use tpuf_v1::euclidian_distance_squared;
+use tpuf_v1::{WalBuffer, WalEntry, buffer_write, buffer_flush, Manifest, query, Vector};
+use uuid::Uuid;
 
-  fn main() {
-      let a = vec![1.0, 2.0, 3.0];
-      let b = vec![4.0, 5.0, 6.0];
-      let d = euclidian_distance_squared(&a, &b);
-      println!("huzzah: {}", d);
-  }
+fn main() {
+    let mut buf = WalBuffer {
+        next_seq: 0,
+        pending: Vec::new(),
+    };
+
+    buffer_write(&mut buf, Uuid::new_v4(), vec![1.0, 0.0, 0.0]);
+    buffer_write(&mut buf, Uuid::new_v4(), vec![0.0, 1.0, 0.0]);
+    buffer_write(&mut buf, Uuid::new_v4(), vec![0.0, 0.0, 1.0]);
+
+    let mut manifest = Manifest {
+        namespace: Uuid::new_v4(),
+        files: Vec::new(),
+    };
+
+    // save vectors before flushing (in order to simulatw what S3 would store)
+    let flushed: Vec<Vector> = buf.pending.iter().map(|e| Vector {
+        id: e.id,
+        values: e.values.clone(),
+    }).collect();
+
+    buffer_flush(&mut buf, &mut manifest);
+
+    let q = vec![0.9, 0.1, 0.0];
+    let results = query(&q, &buf, &flushed, 2);
+
+    println!("query: {:?}", q);
+    for r in &results {
+        println!("  id: {}, distance: {}", r.id, r.distance);
+    }
+}
